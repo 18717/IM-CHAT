c18ec7c4260dfc77ca57d8521976e13bae74d504
一杯香梨 <linli102127@gmail.com>
2022-03-10 23:10:31 +0800

    初始化服务端文件
    初始化服务端文件

--- im_web/server/src/main.js
+++ im_web/server/src/main.js
@@ -1,6 +1,16 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import VueMeta from 'vue-meta';
+import ElementUI, {Message} from 'element-ui';
+import axios from "axios";
+
+Vue.use(ElementUI);
+Vue.use(VueMeta, {
+  keyName: 'head',
+});
+
+Vue.prototype.$axios = axios;
 
 Vue.config.productionTip = false
 
